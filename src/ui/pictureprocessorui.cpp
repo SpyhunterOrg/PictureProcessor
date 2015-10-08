c7062c313f19e6843278f9b370a36bb4e4261fea
Alex <alex@alex.ua>
2015-10-08 22:43:05 +0300

    Renamed PictureProcessor to core Added Qt-project UI Moved to x64

---
+++ src/ui/pictureprocessorui.cpp
@@ -0,0 +1,12 @@
+#include "pictureprocessorui.h"
+
+PictureProcessorUI::PictureProcessorUI(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+PictureProcessorUI::~PictureProcessorUI()
+{
+
+}

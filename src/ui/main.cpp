360d16d192027a2d9754d05159f2ed35f50f1693
Alex <alex@alex.ua>
2015-10-08 23:25:38 +0300

    Cleaned UI project. Removed all files. Preparing to add QML

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,10 +1,10 @@
-#include "pictureprocessorui.h"
 #include <QtWidgets/QApplication>
+#include <QtQml>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    PictureProcessorUI w;
-    w.show();
+
+
     return a.exec();
 }
